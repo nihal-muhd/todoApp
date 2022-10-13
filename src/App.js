fa8f1b9d8a3fa1b31b08c8a6cacc5fc3b615e12a
Nihal Muhammed <nihalmuhammed7025@gmail.com>
2022-10-13 16:36:23 +0530

    completed bro tutorial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [todo, setTodo] = useState('')
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app">
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="subHeading">
+          <br />
+          <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        </div>
+        <div className="input">
+          <input value={todo} onChange={(e) => setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+          <i onClick={() => setTodos([...todos, {id:Date.now(), text: todo, status: false }])} className="fas fa-plus"></i>
+        </div>
+        <div className="todos">
+          {todos.map((obj) => {
+            return (
+              <div className="todo">
+                <div className="left">
+                  <input onChange={(e)=>{
+                    todos.filter((obj2)=>{
+                      if(obj2.id===obj.id){
+                        obj.status=e.target.checked
+                      }
+                      return obj2
+                    })
+                  }} value={obj.status} type="checkbox" name="" id="" />
+                  <p>{obj.text}</p>
+                </div>
+                <div className="right">
+                  <i className="fas fa-times"></i>
+                </div>
+              </div>
+            )
+          })
+          }
+
+        </div>
+      </div>
     </div>
   );
 }
